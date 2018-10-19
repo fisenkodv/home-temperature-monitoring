ae0371d206516fe0e10d43b84419a2245af323bd
Dmitry Fisenko <fisenkodv@gmail.com>
2018-10-18 22:53:18 -0400

    Upgrade Angular

--- src/client/src/app/dashboard/services/temperature.service.ts
+++ src/client/src/app/dashboard/services/temperature.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 export interface DeviceOverview {
   id: string;
