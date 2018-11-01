5f80f137898427c6beb1db092a2f35ceacae45b5
Dmitry Fisenko <fisenkodv@gmail.com>
2018-10-31 22:15:05 -0400

    Add logging

--- src/Monitoring.Web/Monitoring.Api/Program.cs
+++ src/Monitoring.Web/Monitoring.Api/Program.cs
@@ -1,15 +1,44 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Monitoring.Api
 {
+  [UsedImplicitly]
   public class Program
   {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-      CreateWebHostBuilder(args).Build().Run();
+      var currentEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+      var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+        .AddJsonFile($"appsettings.{currentEnv}.json", optional: true, reloadOnChange: false)
+        .AddEnvironmentVariables()
+        .Build();
+
+      Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+      try
+      {
+        Log.Information("Starting Monitoring Web Api Host");
+        CreateWebHostBuilder(args).Build().Run();
+
+        return 0;
+      }
+      catch (Exception ex)
+      {
+        Log.Fatal(ex, "Host terminated unexpectedly");
+        return 1;
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args)
@@ -19,7 +48,7 @@ namespace Monitoring.Api
         .AddJsonFile("hosting.json", optional: true)
         .AddCommandLine(args)
         .Build();
-      
+
       return WebHost.CreateDefaultBuilder(args)
         .UseUrls("http://*:5000")
         .UseConfiguration(config)
