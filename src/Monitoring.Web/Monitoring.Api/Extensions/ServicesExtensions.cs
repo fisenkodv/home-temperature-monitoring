5cc26c9a9240152a5455949cc8bc4bf536b13844
Dmitry Fisenko <fisenkodv@gmail.com>
2018-11-25 21:05:38 -0500

    Update dependencies lifetime

--- src/Monitoring.Web/Monitoring.Api/Extensions/ServicesExtensions.cs
+++ src/Monitoring.Web/Monitoring.Api/Extensions/ServicesExtensions.cs
@@ -7,15 +7,13 @@ namespace Monitoring.Api.Extensions
 {
   public static class ServicesExtensions
   {
-    public static IServiceCollection AddCustomServices(this IServiceCollection services)
+    public static void AddCustomServices(this IServiceCollection services)
     {
-      services.AddScoped<IDeviceRepository, DeviceRepository>();
-      services.AddScoped<IMeasurementRepository, MeasurementRepository>();
+      services.AddTransient<IDeviceRepository, DeviceRepository>();
+      services.AddTransient<IMeasurementRepository, MeasurementRepository>();
 
       services.AddTransient<DeviceService>();
       services.AddTransient<MeasurementService>();
-
-      return services;
     }
   }
 }
